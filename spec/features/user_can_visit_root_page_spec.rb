39b6124cefe4f8ae050e6836c0e454b58b169be8
Jliv316 <jclivingston316@gmail.com>
2018-07-11 09:39:12 -0600

    adds feature test

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
 feature "User can visit root page" do
   scenario "and view navbar contents" do
     visit "/"
     within(".navbar") do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
+      fill_in id="q", with: 80203
+      click_on "Locate"
     end
+    expect(current_path).to eq("/search")
   end
 end
